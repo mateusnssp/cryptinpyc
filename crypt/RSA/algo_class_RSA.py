6aeab8037dac522ef617e2ed76ff45e4bfeb40df
mateusnssp <mateusnss@protonmail.ch>
2020-09-11 13:31:28 -0300

    privateKey

--- crypt/RSA/algo_class_RSA.py
+++ crypt/RSA/algo_class_RSA.py
@@ -1,6 +1,13 @@
 
+# Builtins
 import random
 from math import gcd
+
+
+# Módulos próprios
+from crypt.calc import mod
+
+
 
 def generate_prime_numbers(lim = 100):
     """
@@ -15,13 +22,9 @@ def generate_prime_numbers(lim = 100):
             l.append(num)
     return l
 
-
-
 p, q = random.choice(generate_prime_numbers()), random.choice(generate_prime_numbers())
 print(p, q)
 n = (p-1)*(q-1)
-
-
 
 def phi(expression_n):
     """
@@ -32,6 +35,29 @@ def phi(expression_n):
         if gcd(expression_n, k) == 1:
             amount += 1
     return amount
+
+
+
+
+def generate_e(num = 100):
+    """
+    Gera um numero aleatório e tal que 1 < e <phi(expression_n)
+    """
+    def mdc(n1, n2):
+        rest = 1
+        while n2 != 0:
+            rest = n1 % n2
+            n1 = n2
+            n2 = rest
+        return n1
+    while True:
+        e = random.randrange(2, num)
+        if mdc(num, e) == 1:
+            return e
+
+print(generate_e())
+
+
 
 
 
---
+++ crypt/calc.py
@@ -0,0 +1,7 @@
+def mod(a, b):  # Função mod
+
+    if a < b:
+        return a
+    else:
+        c = a % b
+        return c
