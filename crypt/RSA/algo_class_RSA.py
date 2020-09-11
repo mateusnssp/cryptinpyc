8f9eb7f95172a91ea169687819470d15403c4244
mateusnssp <mateusnss@protonmail.ch>
2020-09-11 12:22:55 -0300

    Update algo_class_RSA.py

--- crypt/RSA/algo_class_RSA.py
+++ crypt/RSA/algo_class_RSA.py
@@ -1,7 +1,25 @@
 
+from math import gcd
+
 class Crypt:
 
-    def __init__(self):
-        pass
+    def __init__(self, n):
+        self.n = n
 
 
+    def phi(self):
+        """
+        Totient
+        :return: phi(n)
+        """
+        amount = 0
+        for k in range(1, self.n + 1):
+            if gcd(self.n, k) == 1:
+                amount += 1
+        return amount
+
+
+if __name__ == '__main__':
+
+    test = Crypt(100)
+    print(test.phi())
