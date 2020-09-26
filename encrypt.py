b35daf9485b5f842eed933687a2a9e33db23770e
mateusnssp <mateusnss@protonmail.ch>
2020-09-25 21:08:33 -0300

    main

---
+++ encrypt.py
@@ -0,0 +1,127 @@
+#IMPORTAÇÕES
+#Builtins
+from datetime import datetime
+import os
+
+#Own
+from precrypt.precrypt import *
+from crypt.RSA.codification import key
+
+
+
+#.....................................RSA......................................
+loop = True
+while loop:
+    try:
+        chave = key()
+
+        loop = False
+    except:
+        loop = True
+
+key = key()
+
+#PRE CRIPTOGRAFAR
+#../../precrypt/caracteres
+
+loop_bar_error = True
+while loop_bar_error:
+    try:
+        msg = str(input("\33[92mINFORME A MENSAGEM........................:\33[m ")) ###RECEBER A MENSAGEM
+        msg_pre_criptografada = precrypt(list(msg), './precrypt/caracteres')
+        loop_bar_error = False
+    except ValueError as error:
+        print(f'\33[31m{error} : BARRA INVERTIDA AINDA NÃO É SUPORTADA.\33[m')
+        loop_bar_error = True
+
+
+
+lista_caracteres_criptografados = []
+i = 0
+for caractere_pre_criptografado in msg_pre_criptografada:
+
+    lista_caracteres_criptografados.append(pow(caractere_pre_criptografado, key[4], mod=key[5]))
+
+
+
+
+
+repetir_pergunta_visualizar = True
+while repetir_pergunta_visualizar:
+    visualizar = input('\33[37mDESEJA VIZUALIZAR O CONTEÚDO GERADO? (Y/N): \33[m')
+
+    if visualizar.upper() == 'Y':
+        print(f'\33[92mCHAVE PÚBLICA.............................:\33[m {f"(n, e) = {chave[2], chave[4]}"}')
+        print(f'\33[92mCHAVE PRIVADA.............................:\33[m {f"(p, q, d) = {chave[0], chave[1], chave[5]}"}')
+        print(f'\33[92mMENSAGEM PRE-CRIPTOGRAFADA:...............:\33[m {msg_pre_criptografada}')
+        print(f'\33[92mMENSAGEM CRIPTOGRAFADA....................:\33[m {lista_caracteres_criptografados}')
+        print(f'\33[92mTAMANHO:..................................:\33[m {len(lista_caracteres_criptografados)}')
+        repetir_pergunta_visualizar = False
+    else:
+        if visualizar.upper() == 'N':
+            repetir_pergunta_visualizar = False
+            pass
+        else:
+            print("\33[31mOPÇÃO DESCONHECIDA, RESPONDA NOVAMENTE.\33[m")
+
+
+### CRIAR ARQUIVO SAVE
+
+
+#CRIAÇÃO DO DIRETÓRIO
+
+path_save_existe = False
+while not path_save_existe:
+    try:
+        path_save = input('\33[37mINFORME O NOME DO DIRETÓRIO...............: \33[m')
+        os.mkdir(path_save)
+        path_save_existe = True
+    except FileExistsError:
+        print('\33[31mDIRETÓRIO JÁ EXISTENTE, PASSE OUTRO NOME.\33[m')
+        path_save_existe = False
+
+
+
+### NOMEAÇÃO DOS ARQUIVOS
+name_file = datetime.now()
+name_file = str(name_file)
+name_file = hex(int(name_file.replace('-', '0').replace(':', '4').replace(' ', '5').replace('.', '7')))
+
+################################
+###CRIAÇÃO DOS ARQUIVOS
+################################
+
+#MENSAGEM CRIPTOGRAFADA
+name_file_saida = path_save + '/' + str(name_file) #Caminho do arquivo ao diretório, já com o nome do mesmo.
+
+arquivo_de_saida = open(name_file_saida, 'w+')
+
+arquivo_de_saida.write(str(lista_caracteres_criptografados).replace('[', '&').replace(']','!').replace(',','x').replace(' ',''))
+
+arquivo_de_saida.close()
+
+
+#############CHAVES#############
+
+
+## CHAVE PÚBLICA
+name_file_key_pb = path_save + '/' + 'pb'
+
+arquivo_key_pb = open(name_file_key_pb, 'w+')
+
+
+arquivo_key_pb.write(f'PB-{chave[2]}>{chave[4]}')
+
+arquivo_key_pb.close()
+
+
+## CHAVE PRIVADA
+name_file_key_pv = path_save + '/' + 'pv'
+
+arquivo_key_pv = open(name_file_key_pv, 'w+')
+
+
+arquivo_key_pv.write(f'PB-{chave[2]}>{chave[4]}&PV-{chave[0]}>{chave[1]}>{chave[5]}')
+
+arquivo_key_pv.close()
+
--- main.py
+++ main.py
@@ -1,130 +1,61 @@
-#IMPORTAÇÕES
 #Builtins
-from datetime import datetime
+from datetime import *
 
-import os
-
-#Own
-from precrypt.precrypt import *
-from crypt.RSA.codification import key
+# Own
 
 
 
 
+time = str(datetime.now())
+time = int(time[11:13])
 
-#.....................................RSA......................................
-loop = True
-while loop:
-    try:
-        chave = key()
+if 5 <= time < 12:
+    greeting = 'Bom dia!'
+if 12 <= time < 18:
+    greeting = 'Boa tarde!'
+if 0 <= time < 5 or 18 <= time <= 24:
+    greeting = 'Boa noite!'
 
-        loop = False
-    except:
-        loop = True
-
-key = key()
-
-#PRE CRIPTOGRAFAR
-#../../precrypt/caracteres
-
-loop_bar_error = True
-while loop_bar_error:
-    try:
-        msg = str(input("\33[92mINFORME A MENSAGEM........................:\33[m ")) ###RECEBER A MENSAGEM
-        msg_pre_criptografada = precrypt(list(msg), './precrypt/caracteres')
-        loop_bar_error = False
-    except ValueError as error:
-        print(f'\33[31m{error} : BARRA INVERTIDA AINDA NÃO É SUPORTADA.\33[m')
-        loop_bar_error = True
+print(f'\n\n\33[92m. . . cryptinpyc . . .\n{str(datetime.now())[0:22]}\n{greeting.center(22, " ")}\n......................\33[m\n\n\n"h" para ajuda;\n"e" para criptografar\n"d" para descriptografar\n\n')
 
 
+def e():
+    e_running = True
+    while e_running:
 
-lista_caracteres_criptografados = []
-i = 0
-for caractere_pre_criptografado in msg_pre_criptografada:
-
-    lista_caracteres_criptografados.append(pow(caractere_pre_criptografado, key[4], mod=key[5]))
-
-
-
-
-
-repetir_pergunta_visualizar = True
-while repetir_pergunta_visualizar:
-    visualizar = input('\33[37mDESEJA VIZUALIZAR O CONTEÚDO GERADO? (Y/N): \33[m')
-
-    if visualizar.upper() == 'Y':
-        print(f'\33[92mCHAVE PÚBLICA.............................:\33[m {f"(n, e) = {chave[2], chave[4]}"}')
-        print(f'\33[92mCHAVE PRIVADA.............................:\33[m {f"(p, q, d) = {chave[0], chave[1], chave[5]}"}')
-        print(f'\33[92mMENSAGEM PRE-CRIPTOGRAFADA:...............:\33[m {msg_pre_criptografada}')
-        print(f'\33[92mMENSAGEM CRIPTOGRAFADA....................:\33[m {lista_caracteres_criptografados}')
-        print(f'\33[92mTAMANHO:..................................:\33[m {len(lista_caracteres_criptografados)}')
-        repetir_pergunta_visualizar = False
-    else:
-        if visualizar.upper() == 'N':
-            repetir_pergunta_visualizar = False
-            pass
+        command_e = input('\33[92mInforme o caminho do arquivo ou pressione "i" para informar manualmente >>> \33[m')
+        if command_e.upper() == "I":
+            msg = input('\33[92mDados: \33[m')
+            e_running = False
         else:
-            print("\33[31mOPÇÃO DESCONHECIDA, RESPONDA NOVAMENTE.\33[m")
+            path = command_e
+            e_running = False
 
 
-### CRIAR ARQUIVO SAVE
+def action():
+    main_running = True
+    while main_running:
 
-
-#CRIAÇÃO DO DIRETÓRIO
-
-path_save_existe = False
-while not path_save_existe:
-    try:
-        path_save = input('\33[37mINFORME O NOME DO DIRETÓRIO...............: \33[m')
-        os.mkdir(path_save)
-        path_save_existe = True
-    except FileExistsError:
-        print('\33[31mDIRETÓRIO JÁ EXISTENTE, PASSE OUTRO NOME.\33[m')
-        path_save_existe = False
+        command = input('\33[92m[cryptinpyc] >>> \33[m')
 
 
 
-### NOMEAÇÃO DOS ARQUIVOS
-name_file = datetime.now()
-name_file = str(name_file)
-name_file = hex(int(name_file.replace('-', '0').replace(':', '4').replace(' ', '5').replace('.', '7')))
+        if command.upper() == 'H':
+            print('\nHELP')
+        if command.upper() == 'E':
+            # from encrypt import *
+            e()
 
-################################
-###CRIAÇÃO DOS ARQUIVOS
-################################
+        if command.upper() == 'D':
+            pass
 
-#MENSAGEM CRIPTOGRAFADA
-name_file_saida = path_save + '/' + str(name_file) #Caminho do arquivo ao diretório, já com o nome do mesmo.
+        if command.upper() == 'h':
+            pass
 
-arquivo_de_saida = open(name_file_saida, 'w+')
-
-arquivo_de_saida.write(str(lista_caracteres_criptografados).replace('[', '&').replace(']','!').replace(',','x').replace(' ',''))
-
-arquivo_de_saida.close()
+        if command.upper().count('EXIT') > 0:
+            main_running = False
+            exit()
 
 
-#############CHAVES#############
 
-
-## CHAVE PÚBLICA
-name_file_key_pb = path_save + '/' + 'pb'
-
-arquivo_key_pb = open(name_file_key_pb, 'w+')
-
-
-arquivo_key_pb.write(f'PB-{chave[2]}>{chave[4]}')
-
-arquivo_key_pb.close()
-
-
-## CHAVE PRIVADA
-name_file_key_pv = path_save + '/' + 'pv'
-
-arquivo_key_pv = open(name_file_key_pv, 'w+')
-
-
-arquivo_key_pv.write(f'PB-{chave[2]}>{chave[4]}&PV-{chave[0]}>{chave[1]}>{chave[5]}')
-
-arquivo_key_pv.close()
-
+action()
