94fa007ef9c8912302584f9bd053577f4ca2bb16
Mateus Nunes <mateusnssp@protonmail.ch>
2020-10-04 09:52:41 -0300

    Update conversion.py

--- pre/conversion.py
+++ pre/conversion.py
@@ -5,10 +5,16 @@ def precrypt(conteudo):
         conteudo[conteudo.index(i)] = ord(i)
     return conteudo
 
+def de_precrypt(conteudo):
+    conteudo = conteudo
+    for i in conteudo:
+        conteudo[conteudo.index(i)] = chr(i)
+    return conteudo
 
 
 if __name__ == '__main__':
-    print(precrypt('rsdddddddatifuyaseiufgxagfbcandyafsdatsdtd437985294285nhc385691236cx1b689s3n'))
+    print(precrypt('Loren'))
+    print(de_precrypt([76, 111, 114, 101, 110]))
 
 
 
