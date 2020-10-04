3b739c729bccb3322e6bc0efdfe3fe449b0e5fff
Mateus Nunes <mateusnssp@protonmail.ch>
2020-10-04 09:30:08 -0300

    Update conversion.py

--- pre/conversion.py
+++ pre/conversion.py
@@ -1,35 +1,15 @@
-def precrypt(conteudo, path_file):
-    """
-    Pré-criptografar conteudo.
-    conteudo deve ser do tipo <class 'list'>
-    """
 
-    # Abrir banco de caracteres.
-    l = open(path_file)
-
-
-
-
-    # Passar banco de caracteres para variável do programa
-    caracteres = [x for x in l.read()]
-
-    # Fechar banco de caracteres
-    l.close()
-
-
-    # Definir itens correspodentes para realizar as substituições
-    # Padrão obrigatório do momento (12-07-2020): len(str(codigo[1])) = 4
-    codigo = [i for i in range(1000, 1000 + len(caracteres))]
-
-
-    # Realizar as substituições
+def precrypt(conteudo):
+    conteudo = list(conteudo)
     for i in conteudo:
-        conteudo[conteudo.index(i)] = codigo[caracteres.index(i)]
-
-
+        conteudo[conteudo.index(i)] = ord(i)
     return conteudo
 
 
+
 if __name__ == '__main__':
-    print(precrypt(list('mmm'), 'caracteres'))
+    print(precrypt('rsdddddddatifuyaseiufgxagfbcandyafsdatsdtd437985294285nhc385691236cx1b689s3n'))
+
+
+
 
