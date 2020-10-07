7688a622fdfd5fe12fa3bb7f18474f082c1310be
Mateus Nunes <mateusnss@protonmail.ch>
2020-10-07 12:06:14 -0300

    Update read.py

--- read.py
+++ read.py
@@ -5,7 +5,30 @@ class Read:
     """
     def __init__(self, path):
 
-        self.path = path
+        self.path = path if path[-1] == '/' else f'{path}/'
+
+        self.dado = self.read_dado()
+        self.pb = self.read_pb()
+        self.pv = self.read_pv()
+
+    def read_dado(self):
+        with open(f'{self.path}ief', 'r+') as file:
+            dado = file.read()
+        return dado
+
+    def read_pb(self):
+        with open(f'{self.path}pb', 'r+') as file:
+            pv = file.read()
+        return pv
+
+    def read_pv(self):
+        with open(f'{self.path}pv', 'r+') as file:
+            pv = file.read()
+        return pv
 
 
-
+if __name__ == '__main__':
+    test = Read('./66c')
+    print(test.dado)
+    print(test.pb)
+    print(test.pv)
