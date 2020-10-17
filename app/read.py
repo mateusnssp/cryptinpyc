77b93afd85363fefca266c9b497c6bae82f093eb
Mateus Nunes <mateusnss@protonmail.ch>
2020-10-17 11:30:29 -0300

    read.py completo

--- app/read.py
+++ app/read.py
@@ -11,6 +11,21 @@ class Read:
         self.pb = self.read_pb()
         self.pv = self.read_pv()
 
+
+
+        # Variáveis inverticad conforme as de gfile.py
+        self.hash_l2 = list("0123456789[], ()")
+        self.hash_l1 = list("01f3456789harbyk")
+
+
+        self.dado_traduzido = self.traduzir_dado(self.dado, self.hash_l1, self.hash_l2)
+        self.pb_traduzido = self.traduzir_pb(self.pb, self.hash_l1, self.hash_l2)
+        self.pv_traduzido = self.traduzir_pv(self.pv, self.hash_l1, self.hash_l2)
+
+
+
+
+
     def read_dado(self):
         with open(f'{self.path}ief', 'r+') as file:
             dado = file.read()
@@ -27,8 +42,50 @@ class Read:
         return pv
 
 
+
+
+
+
+
+    def traduzir_dado(self, dado, hash_l1, hash_l2):
+        dado = str(dado)
+        for char in dado:
+            if dado.count(char) > 0:
+                dado = dado.replace(char, str(hash_l2[hash_l1.index(char)]))
+
+        dado = f'{dado}'
+        return dado
+
+
+    def traduzir_pb(self, pb, hash_l1, hash_l2):
+        pb = str(pb)
+        for char in pb:
+            if pb.count(char) > 0:
+                pb = pb.replace(char, str(hash_l2[hash_l1.index(char)]))
+
+        pb = f'{pb}'
+        return pb
+
+
+
+    def traduzir_pv(self, pv, hash_l1, hash_l2):
+        pv = str(pv)
+        for char in pv:
+            if pv.count(char) > 0:
+                pv = pv.replace(char, str(hash_l2[hash_l1.index(char)]))
+
+        pv = f'{pv}'
+        return pv
+
+
 if __name__ == '__main__':
-    test = Read('./c0e')
+    test = Read('./5a9')
     print(test.dado)
     print(test.pb)
     print(test.pv)
+
+
+
+    print(test.dado_traduzido)
+    print(test.pb_traduzido)
+    print(test.pv_traduzido)
